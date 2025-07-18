24c7ec59cd84cda7338ee157bcdf64898ff1f877
Yohei Yamada <morikubo827@gmail.com>
2025-07-18 23:05:39 +0900

    em

---
+++ em.py
@@ -0,0 +1,24 @@
+from scipy.stats import norm
+
+x = [1.3, 1.5, 1.6, 1.7, 4.7, 5.0, 5.2, 5.5]
+n = len(x)
+
+pi = 0.5
+mu_1 = 1
+mu_2 = 6
+
+for i in range(20):
+
+    gamma_1 = []
+    gamma_2 = []
+    for xi in x:
+        gamma_1.append(pi*norm.pdf(xi, mu_1, 1)
+                       / (pi*norm.pdf(xi, mu_1, 1) + (1-pi)*norm.pdf(xi, mu_2, 1)))
+        gamma_2.append((1-pi)*norm.pdf(xi, mu_2, 1)
+                       / (pi*norm.pdf(xi, mu_1, 1) + (1-pi)*norm.pdf(xi, mu_2, 1)))
+
+    pi = sum(gamma_1) / n
+    mu_1 = sum([x * y for (x, y) in zip(gamma_1, x)]) / sum(gamma_1)
+    mu_2 = sum([x * y for (x, y) in zip(gamma_2, x)]) / sum(gamma_2)
+
+    print(i+1, pi, mu_1, mu_2)
