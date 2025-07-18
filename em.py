c583e548fb065755ad276a1cf3a992b5c1fc0387
Yohei Yamada <morikubo827@gmail.com>
2025-07-18 23:14:42 +0900

    em 修正

--- em.py
+++ em.py
@@ -6,19 +6,24 @@ n = len(x)
 pi = 0.5
 mu_1 = 1
 mu_2 = 6
+sigma_1 = 1
+sigma_2 = 1
 
 for i in range(20):
 
     gamma_1 = []
     gamma_2 = []
     for xi in x:
-        gamma_1.append(pi*norm.pdf(xi, mu_1, 1)
-                       / (pi*norm.pdf(xi, mu_1, 1) + (1-pi)*norm.pdf(xi, mu_2, 1)))
-        gamma_2.append((1-pi)*norm.pdf(xi, mu_2, 1)
-                       / (pi*norm.pdf(xi, mu_1, 1) + (1-pi)*norm.pdf(xi, mu_2, 1)))
+        gamma_1.append(pi*norm.pdf(xi, mu_1, sigma_1)
+                       / (pi*norm.pdf(xi, mu_1, sigma_1) + (1-pi)*norm.pdf(xi, mu_2, sigma_2)))
+        gamma_2.append((1-pi)*norm.pdf(xi, mu_2, sigma_2)
+                       / (pi*norm.pdf(xi, mu_1, sigma_1) + (1-pi)*norm.pdf(xi, mu_2, sigma_2)))
 
     pi = sum(gamma_1) / n
     mu_1 = sum([x * y for (x, y) in zip(gamma_1, x)]) / sum(gamma_1)
     mu_2 = sum([x * y for (x, y) in zip(gamma_2, x)]) / sum(gamma_2)
 
     print(i+1, pi, mu_1, mu_2)
+
+print(gamma_1)
+print(gamma_2)
